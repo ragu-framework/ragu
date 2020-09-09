0b3c8f95dad7b16aafb1b32ff668d27c99c265eb
Maniero <cmaniero@thoughtworks.com>
2020-09-09 20:20:23 -0300

    compiles project before run

--- demo/catalog/components/featured-products/index.tsx
+++ demo/catalog/components/featured-products/index.tsx
@@ -1,50 +1,12 @@
 import * as React from "react";
-import ReactDOM from 'react-dom'
-import {renderToString} from 'react-dom/server';
+import {createReactRaguComponent} from "../../ragu-react-helper";
 
 interface Props {
   name: string;
 }
 
-class MyComponent extends React.Component<Props, Props> {
-  state = {...this.props};
+const MyComponent = (props: Props) => <div>Hello, {props.name}</div>;
 
-  render() {
-    return <div>Hello, {this.state.name} <button onClick={this.changeNameToWorld.bind(this)}>call me world</button></div>
-  }
-
-  private changeNameToWorld() {
-    this.setState({name: 'World'});
-  }
-}
-
-export default {
-  dependencies: [
-    {
-      nodeRequire: 'react',
-      globalVariable: 'React',
-      dependency: 'https://unpkg.com/react@16/umd/react.production.min.js'
-    },
-    {
-      nodeRequire: 'react-dom',
-      globalVariable: 'ReactDOM',
-      dependency: 'https://unpkg.com/react-dom@16/umd/react-dom.production.min.js'
-    },
-    {
-      nodeRequire: 'react-dom/server',
-      globalVariable: 'ReactDOM',
-      dependency: 'https://unpkg.com/react-dom@16/umd/react-dom.production.min.js'
-    }
-  ],
-  render(props: Props) {
-    return {
-      state: {},
-      html: renderToString(
-          React.createElement(MyComponent, props)
-      )
-    }
-  },
-  hydrate(element: HTMLElement, props: Props) {
-    ReactDOM.hydrate(<MyComponent name={props.name} />, element);
-  }
-}
+export default createReactRaguComponent<Props>(
+    (props) => <MyComponent name={props.name} />
+)
