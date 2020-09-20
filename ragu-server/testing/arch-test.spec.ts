26a8f4223ab446567cccf2e846fab89a64da9144
Maniero <cmaniero@thoughtworks.com>
2020-09-20 01:08:35 -0300

    exposes all modules in index.ts and writes a test to avoid forgetting to add them

---
+++ ragu-server/testing/arch-test.spec.ts
@@ -0,0 +1,38 @@
+import * as fs from "fs";
+import * as path from "path";
+
+const expectExposesAllFilesOfDirectory = (directoryName: string) => {
+  const compilerFiles = fs.readdirSync(path.join(__dirname, '..', 'src', directoryName), { withFileTypes: true })
+      .filter(directoryEntry => directoryEntry.isFile())
+      .map(directoryEntry => directoryEntry.name);
+
+  const indexContent = fs.readFileSync(path.join(__dirname, '..', 'index.ts')).toString();
+
+  for (let fileName of compilerFiles) {
+    const baseDirectory = directoryName === '.' ? '' : `${directoryName}/`;
+
+    expect(indexContent).toContain(`export * from './src/${baseDirectory}${fileName.replace('.ts', '')}';`);
+  }
+}
+
+describe('Architecture tests', () => {
+  it('exposes all files of root module into index.ts', () => {
+    expectExposesAllFilesOfDirectory('.');
+  });
+
+  it('exposes all files of compiler module into index.ts', () => {
+    expectExposesAllFilesOfDirectory('compiler');
+  });
+
+  it('exposes all files of logging module into index.ts', () => {
+    expectExposesAllFilesOfDirectory('logging');
+  });
+
+  it('exposes all files of preview module into index.ts', () => {
+    expectExposesAllFilesOfDirectory('preview');
+  });
+
+  it('exposes all files of ssr module into index.ts', () => {
+    expectExposesAllFilesOfDirectory('ssr');
+  });
+});
