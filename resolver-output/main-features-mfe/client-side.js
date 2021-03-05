d14957417cf074d55efd04d31d0b12eae0609277
carlosmaniero <carlosmaniero@gmail.com>
2021-03-05 12:24:55 +0000

    Deploying to gh-pages from  @ 1d28823b220e6ec9f1f1fef552bf1d3d309160aa 🚀

--- resolver-output/main-features-mfe/client-side.js
+++ resolver-output/main-features-mfe/client-side.js
@@ -1,5 +1,5 @@
 
-      var component = require('/home/runner/work/ragu/ragu/main-features-mfe/index');
+      var component = require('/home/runner/work/ragu/ragu/main-features-mfe/main-features-mfe');
       var resolver = require('/home/runner/work/ragu/ragu/main-features-mfe/node_modules/ragu-simple-adapter/resolvers/hydrate-resolver');
 
       module.exports.default = (resolver.default || resolver)(component.default || component);
