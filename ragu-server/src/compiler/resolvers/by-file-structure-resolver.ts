05c61ffe88d7c541f151ee6bd9f766b580754c95
Maniero <carlosmaniero@gmail.com>
2021-03-21 02:46:34 -0300

    gets filename for files

--- ragu-server/src/compiler/resolvers/by-file-structure-resolver.ts
+++ ragu-server/src/compiler/resolvers/by-file-structure-resolver.ts
@@ -6,16 +6,13 @@ import {ComponentResolver} from "./component-resolver";
 export class ByFileStructureComponentResolver extends ComponentResolver {
   private static instance: ComponentResolver;
 
-  componentList(): Promise<string[]> {
-    return new Promise((resolve, reject) => {
-      fs.readdir(this.config.components.sourceRoot, (err, files) => {
-        if (err) {
-          reject(err);
-          return
-        }
-        resolve(files);
-      });
-    });
+  async componentList(): Promise<string[]> {
+      const files: string[] = await fs.promises.readdir(this.config.components.sourceRoot);
+      return await Promise.all(files.map(async (filename) => {
+        const stat = await fs.promises.lstat(path.join(this.config.components.sourceRoot, filename));
+
+        return stat.isDirectory() ? filename : path.parse(filename).name;
+      }));
   }
 
   async componentClientSidePath(componentName: string): Promise<string> {
